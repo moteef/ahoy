528c587920c02f62f2cad59d9b55a26e3d5611e5
paulo <paulobapmar@gmail.com>
2021-01-04 05:36:14 +0000

    Serve ahoy from `/api/v1/*`

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
+# For frexit we need to temporarly serve Ahoy from '/ahoy/*' (legacy) and from
+# '/api/v1/ahoy'. When frexit is done only '/api/v1/ahoy' should be served.
+
 Rails.application.routes.draw do
-  mount Ahoy::Engine => "/ahoy" if Ahoy.api
+  mount Ahoy::Engine => "/" if Ahoy.api
 end
 
 Ahoy::Engine.routes.draw do
-  scope module: "ahoy" do
+  scope path: 'ahoy', module: 'ahoy' do
+    resources :visits, only: [:create]
+    resources :events, only: [:create]
+  end
+
+  scope path: 'api/v1/ahoy', module: 'ahoy' do
     resources :visits, only: [:create]
     resources :events, only: [:create]
   end
